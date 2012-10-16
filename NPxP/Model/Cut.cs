cc0f6d34c5e04f47a4c145daf9ac413417209206
AndyYou <andyyu0920@gmail.com>
2012-10-16 11:26:41 +0800

    Add New Cut model

---
+++ NPxP/Model/Cut.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NPxP.Model
+{
+    public class Cut
+    {
+        public double MD { set; get; }
+        public bool IsDealed { set; get; }
+
+        public Cut(double md)
+        {
+            this.MD = md;
+            this.IsDealed = false ; // Default is false.
+        }
+    }
+}
